c4333248f4392fe8ea89c2432afbbf1eb9bd39c9
cosmin <cosmin.popescu93@gmail.com>
2021-03-16 21:26:49 +0200

    Reorganised in menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,45 @@
-<script>
+<script xmlns="http://www.w3.org/1999/html">
 	import Dog from './Dog.svelte'
 	import Users from './Users.svelte'
 
-	let users = [
-		{id:1,name: "king"},
-		{id:2, name: "gowtham"},
-		{id:3, name: "sai"}
-	]
+	export let menu = 1;
 </script>
 
-<main>
-	<div id = "users">
-		<Users/>
-	</div>
-	<div>
-		<h1>Random dogs</h1>
-		<Dog/>
-	</div>
-	<ul>
-		{#each users as user}
-			<li>{user.id} - {user.name}</li>
+<ul id="menu">
+	<li><a href="Dog.svelte" on:click|preventDefault={() => (menu = 1)}>Dog</a></li> |
+	<li><a href="Users.svelte" on:click|preventDefault={() => (menu = 2)}>Users</a></li>
+</ul>
 
-		{/each}
-	</ul>
+{#if menu === 1}
+	<Dog />
+{:else if menu === 2}
+	<Users />
+{:else}
+	<h1>
+		Page Not Found
+	</h1>
+{/if}
+<main>
+<!--	<div id = "users">-->
+<!--		<Users/>-->
+<!--	</div>-->
+<!--	<div>-->
+<!--		<h1>Random dogs</h1>-->
+<!--		<Dog/>-->
+<!--	</div>-->
+<!--	<ul>-->
+<!--		{#each users as user}-->
+<!--			<li>{user.id} - {user.name}</li>-->
+
+<!--		{/each}-->
+<!--	</ul>-->
+
 </main>
 
 <style>
+	ul#menu li{
+		display : inline;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
---
+++ src/Menu.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Users from "./Users.svelte";
+    import Dog from "./Dog.svelte";
+
+    export let menu = 1;
+</script>
+
+<style>
+    ul#menu li{
+        display : inline;
+    }
+</style>
+
+<ul id="menu">
+    <li><a href="Dog.svelte" on:click|preventDefault={() => (menu = 1)}>Dog</a></li> |
+    <li><a href="Users.svelte" on:click|preventDefault={() => (menu = 2)}>Users</a></li>
+</ul>
+
+{#if menu === 1}
+    <Dog />
+{:else if menu === 2}
+    <Users />
+{:else}
+    <h1>
+        Page Not Found
+    </h1>
+{/if}

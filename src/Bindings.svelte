da3a6076a75a0b4d549da709afd71b7865d384ae
cosmin <cosmin.popescu93@gmail.com>
2021-03-24 07:31:35 +0200

    More on Bindings

--- src/Bindings.svelte
+++ src/Bindings.svelte
@@ -5,6 +5,53 @@
     let b = 2
 
     let yes = false
+
+    let scoops = 1
+    let flavours = ['Mint choc chip']
+
+    let menu = [
+        'Cookies and cream',
+        'Mint choc chip',
+        'Raspberry ripple'
+    ]
+
+    function join(flavours) {
+        if (flavours.length === 1) {
+            return flavours[0]
+        }
+        return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`
+    }
+
+    let questions = [
+        { id: 1, text: `Where did you go to school?` },
+        { id: 2, text: `What is your mother's name?` },
+        { id: 3, text: `What is another personal fact that an attacker could easily find with Google?` }
+    ]
+
+    let selected
+    let answer = ''
+
+    function handleSubmit() {
+        alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`)
+    }
+
+    let html = '<p>Write some text!</p>'
+
+    let todos = [
+        { done: false, text: 'finish Svelte tutorial' },
+        { done: false, text: 'build an app' },
+        { done: false, text: 'world domination' }
+    ]
+
+    function add() {
+        todos = todos.concat({ done: false, text: '' })
+    }
+
+    function clear() {
+        todos = todos.filter(t => !t.done)
+    }
+
+    $: remaining = todos.filter(t => !t.done).length
 </script>
 
 <article>
@@ -48,4 +95,131 @@
 
 <button disabled={!yes}>
     Subscribe
+</button>
+
+
+<p>
+    bind:group - inputs relating to the same value
+    Radio inputs in the same group are mutually exclusive; checkbox inputs in the same group form an array of selected values.
+</p>
+<h2>Size</h2>
+
+<label>
+    <input type=radio bind:group={scoops} value={1}>
+    One scoop
+</label>
+
+<label>
+    <input type=radio bind:group={scoops} value={2}>
+    Two scoops
+</label>
+
+<label>
+    <input type=radio bind:group={scoops} value={3}>
+    Three scoops
+</label>
+
+<h2>Flavours</h2>
+
+<select multiple bind:value={flavours}>
+    {#each menu as flavour}
+        <option value={flavour}>
+            {flavour}
+        </option>
+    {/each}
+</select>
+<!--{#each menu as flavour}-->
+<!--    <label>-->
+<!--        <input type=checkbox bind:group={flavours} value={flavour}>-->
+<!--        {flavour}-->
+<!--    </label>-->
+<!--{/each}-->
+
+
+{#if flavours.length === 0}
+    <p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+    <p>Can't order more flavours than scoops!</p>
+{:else}
+    <p>
+        You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
+        of {join(flavours)}
+    </p>
+{/if}
+
+<style>
+    input { display: block; width: 500px; max-width: 100%; }
+    [contenteditable] {
+        padding: 0.5em;
+        border: 1px solid #eee;
+        border-radius: 4px;
+    }
+    .done {
+        opacity: 0.4;
+    }
+</style>
+
+
+
+<p>
+    bind:select
+    Note that the option values are objects rather than strings. Svelte doesn't mind.
+    Because we haven't set an initial value of selected, the binding will set it to the default value (the first in the list) automatically.
+    Be careful though — until the binding is initialised, selected remains undefined, so we can't blindly reference e.g. selected.id in the template.
+</p>
+<h2>Insecurity questions</h2>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <select bind:value={selected} on:change="{() => answer = ''}">
+        {#each questions as question}
+            <option value={question}>
+                {question.text}
+            </option>
+        {/each}
+    </select>
+
+    <input bind:value={answer}>
+
+    <button disabled={!answer} type=submit>
+        Submit
+    </button>
+</form>
+
+<p>selected question {selected ? selected.id : '[waiting...]'}</p>
+
+<p>
+    Elements with a contenteditable="true" attribute support textContent and innerHTML bindings:
+</p>
+<div contenteditable="true" bind:innerHTML={html}></div>
+
+<pre>{html}</pre>
+
+
+<p>
+    we can use bind inside each blocks.
+</p>
+<h1>Todos</h1>
+
+{#each todos as todo}
+    <div class:done={todo.done}>
+        <input
+                type=checkbox
+                bind:checked={todo.done}
+        >
+
+        <input
+                placeholder="What needs to be done?"
+                bind:value={todo.text}
+        >
+    </div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+    Add new
+</button>
+
+<button on:click={clear}>
+    Clear completed
 </button>
